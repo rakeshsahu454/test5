cf5de1cc1e97eadeef9f5905e1685712a941952a
Rakeshgit config --global user.email rakeshkumarsahu36843@gmail.com <rakeshkumarsahu36843@gmail.com>
2025-04-22 10:19:53 +0530

    First commit

---
+++ src/test/java/in/psa/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.psa.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
