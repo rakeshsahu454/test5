bdd1779e2c338ed333bf1884d25d81c362bc80a9
rakeshsahu454 <rakeshkumarsahu36843@gmail.com>
2025-04-22 15:28:48 +0530

    Update TestApplication.java

--- src/main/java/in/psa/test/TestApplication.java
+++ src/main/java/in/psa/test/TestApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x =100;
+		int y=200;
+		double z=10.3;
 		SpringApplication.run(TestApplication.class, args);
 	}
 
